f84163938856b3725a5bf3b663f644927d56d510
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2025-04-01 16:47:02 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "Mimi_98"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package Mimi_98
+
+fun main() {
+    println("Hello World!")
+}
