b235c049b385d4429d034872dc314556fe0d3e12
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2025-04-05 19:53:35 +0300

    added gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.20"
+    application
 }
 
-group = "Mimi_98"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,12 +10,20 @@ repositories {
 }
 
 dependencies {
+
+    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.1.20")
     testImplementation(kotlin("test"))
+
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
 }
