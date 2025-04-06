da5c7d04345c19d0532b3cb7d270a2f7121d6e4c
MiMi <134426979+MiMi-A98@users.noreply.github.com>
2025-04-06 18:07:08 +0300

    refactored code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,19 @@
-import Mimi_98.FileManager
+import Mimi_98.PublicDeclarationPrinter
 import java.io.File
+import java.io.FileNotFoundException
 
 fun main(args: Array<String>) {
 
-    if (args.isEmpty()) {
-        println("Error: Empty parameter for main method!")
-        return
-    }
+    require(args.isNotEmpty()) { "Error: Missing required target directory path parameter" }
 
     val sourceDirectory = File(args[0])
-    if (!sourceDirectory.exists() || !sourceDirectory.isDirectory) {
-        println("Error: Provided path '${sourceDirectory.path}' is not a directory or it does not exists")
-        return
+
+    require(sourceDirectory.isDirectory) { "Error: Provided path is not a directory" }
+
+    if (!sourceDirectory.exists()) {
+        throw FileNotFoundException("Error: Provided path does not exists")
     }
 
-    val fileManager = FileManager()
-    fileManager.processKotlinFiles(sourceDirectory)
+    PublicDeclarationPrinter().print(sourceDirectory)
 }
 
